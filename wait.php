623aa99b1571167ec5b292ad95ab05241fb3f6be
Maurits van der Schee <maurits@vdschee.nl>
2020-08-04 17:20:04 +0200

    fix

--- wait.php
+++ wait.php
@@ -3,7 +3,7 @@
 $hash = $_SERVER['PATH_INFO'];
 $date = gmdate("Y-m-d");
 if (!file_exists("done/$date/$hash")) {
-    die('<meta http-equiv="refresh" content="1"><form action="./">Wait.. <input type="submit" value="Cancel".></form>');
+    die('<meta http-equiv="refresh" content="1"><form action="../">Wait.. <input type="submit" value="Cancel".></form>');
 }
 
 header('Location: show.php/' . $hash);
