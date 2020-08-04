f3ca18bcd47a996ad9e6ebdb7247aa495713a5c0
Maurits van der Schee <maurits@vdschee.nl>
2020-08-05 01:04:04 +0200

    fix

--- header.php
+++ header.php
@@ -8,3 +8,4 @@
 </head>
 
 <body>
+<a href="https://github.com/mevdschee/gdpr-scanner"><img src="forkme_right_darkblue_121621.svg" style="position:absolute;top:0;right:0;" alt="Fork me on GitHub"></a>
