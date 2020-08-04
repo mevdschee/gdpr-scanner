e6f384e07873c0599e6c956754b25315ba2bd154
Maurits van der Schee <maurits@vdschee.nl>
2020-08-05 01:31:34 +0200

    fix

--- footer.php
+++ footer.php
@@ -1,4 +1,8 @@
-<hr/>
-<p style="color:silver">(c) Copyright 2020 - Maurits van der Schee - Read the <a href="https://tqdev.com/">TQdev Blog</a> for more info</p>
+<p><br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<a href="https://tqdev.com/">TQdev Blog</a></p>
 </body>
 </html>
