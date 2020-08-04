2c0996a5084bea75e389014e3f0b2c2aea53cf32
Maurits van der Schee <maurits@vdschee.nl>
2020-08-05 01:33:00 +0200

    fix

--- footer.php
+++ footer.php
@@ -1,8 +1,2 @@
-<p><br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<a href="https://tqdev.com/">TQdev Blog</a></p>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php include 'header.php';?>
 <a href="https://github.com/mevdschee/gdpr-scanner"><img src="forkme_right_darkblue_121621.svg" style="position:absolute;top:0;right:0;" alt="Fork me on GitHub"></a>
 <h1>GDPR scanner</h1>
+<p>For more information, read the <a href="https://tqdev.com/">TQdev Blog</a> about the scanner.</p>
 <form action="new.php" method="post">
     <label for="url">URL</label><br />
     <input id="url" type="text" name="url" />
