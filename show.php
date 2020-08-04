cb2313fdf5af4b971120df3523c9e7a10b889e8f
Maurits van der Schee <maurits@vdschee.nl>
2020-08-04 23:06:45 +0200

    fix

--- show.php
+++ show.php
@@ -29,11 +29,11 @@ $reponse = json_decode(file_get_contents("done/$date/$hash"), true);
 <h1><?php echo $reponse['url']; ?></h1>
 <table cellspacing="0">
     <tr>
-        <th>Domain</th>
-        <th>Ping &nbsp;</th>
-        <th>EU</th>
-        <th>Country</th>
-        <th>Organization</th>
+        <th>Domain<sup>1</sup></th>
+        <th>Ping<sup>2</sup></th>
+        <th>EU<sup>3</sup></th>
+        <th>Country<sup>4</sup></th>
+        <th>Organization<sup>5</sup></th>
     </tr>
     <?php foreach ($reponse['lines'] as $line) : ?>
         <tr>
@@ -43,6 +43,13 @@ $reponse = json_decode(file_get_contents("done/$date/$hash"), true);
         </tr>
     <?php endforeach; ?>
 </table>
+<ol>
+    <li>Domain to which connections where made</li>
+    <li>Latency to the domain in milliseconds</li>
+    <li>EU country for domain's IP address</li>
+    <li>Country of the domain's IP address</li>
+    <li>Organization of the domain's IP address</li>
+</ol>
 <br />
 <form action="../"><input type="submit" value="Close" /></form>
 <?php include 'footer.php'; ?>
