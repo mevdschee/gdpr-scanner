8870b1dd2e07ebc218408ce4a394ce268e9acf27
Maurits van der Schee <maurits@vdschee.nl>
2020-08-04 22:50:00 +0200

    fix

--- index.php
+++ index.php
@@ -1,20 +1,9 @@
-<html>
-
-<head>
-    <title>GDPR scanner</title>
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-</head>
-
-<body>
-    <h1>GDPR scanner</h1>
-    <form action="new.php" method="post">
-        <label for="url">URL</label><br />
-        <input id="url" type="text" name="url" />
-        <br /><br />
-        <input type="submit" value="Scan" />
-    </form>
-</body>
-
-</html>
+<?php include 'header.php'; ?>
+<h1>GDPR scanner</h1>
+<form action="new.php" method="post">
+    <label for="url">URL</label><br />
+    <input id="url" type="text" name="url" />
+    <br /><br />
+    <input type="submit" value="Scan" />
+</form>
+<?php include 'footer.php'; ?>
--- wait.php
+++ wait.php
@@ -1,12 +1,18 @@
 <?php
-
 $hash = trim($_SERVER['PATH_INFO'], '/');
 $date = gmdate("Y-m-d");
 if (file_exists("todo/$hash")) {
-    die('<meta http-equiv="refresh" content="1"><form action="../">Wait.. <input type="submit" value="Cancel"/></form>');
+    $text = 'Wait...';
+} elseif (!file_exists("done/$date/$hash")) {
+    $text = 'Executing...';
+} else {
+    die(header('Location: ../show.php/' . $hash));
 }
-if (!file_exists("done/$date/$hash")) {
-    die('<meta http-equiv="refresh" content="1"><form action="../">Executing.. <input type="submit" value="Cancel"/></form>');
-}
-
-header('Location: ../show.php/' . $hash);
+?>
+<?php include 'header.php'; ?>
+<form action="../">
+    <meta http-equiv="refresh" content="1">
+    Wait...
+    <input type="submit" value="Cancel" />
+</form>
+<?php include 'footer.php'; ?>
