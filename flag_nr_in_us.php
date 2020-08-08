4a141a1ab871102e19bd8792c9d796f8b10bf143
Maurits van der Schee <maurits@vdschee.nl>
2020-08-08 16:25:50 +0200

    fix

---
+++ flag_nr_in_us.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+<h1>New Relic Application Monitoring</h1>
+<p>
+    New Relic has seperate domains for Browser Application Monitoring monitoring in the European Union. See:
+</p>
+<ul>
+    <li><a href="https://docs.newrelic.com/docs/using-new-relic/cross-product-functions/install-configure/networks#browser-specific">New Relic Networks: Browser domains</a></li>
+</ul>
+<?php include 'footer.php'; ?>
