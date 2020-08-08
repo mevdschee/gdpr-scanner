d912f8f60f309b9443548facdf7eae2abe949522
Maurits van der Schee <maurits@vdschee.nl>
2020-08-08 17:36:53 +0200

    fix

---
+++ flag_fb_connect.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+<h1>Facebook Connect</h1>
+<p>
+    Before people click the facebook connect/login button, their IP address and user agent is already shared with Facebook.
+</p>
+<ul>
+    <li><a href="https://developers.facebook.com/docs/facebook-login/web/login-button?locale=en_US">Facebook for developers - Login Button</a></li>
+</ul>
+<?php include 'footer.php'; ?>
---
+++ flag_g_dc_ads.php
@@ -0,0 +1,10 @@
+<?php include 'header.php'; ?>
+<h1>Google DoubleClick Ads</h1>
+<p>
+    Ads are most likely not required for the functionality of the site and thus sharing IP address and user agent with Google for this purpose requires consent.
+</p>
+<ul>
+    <li><a href="https://policies.google.com/technologies/partner-sites">Google - Privacy and Terms</a></li>
+    <li><a href="https://www.google.com/about/company/user-consent-policy/">Google - EU user consent policy</a></li>
+</ul>
+<?php include 'footer.php'; ?>
