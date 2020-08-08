02945c4165350120c6de2146512c473a8f424792
Maurits van der Schee <maurits@vdschee.nl>
2020-08-08 17:08:15 +0200

    fix

--- flag_g_fonts.php
+++ flag_g_fonts.php
@@ -1,7 +1,9 @@
-<?php include 'header.php';?>
+<?php include 'header.php'; ?>
 <h1>Google Fonts</h1>
 <p>
-    On this <a href="https://github.com/google/fonts/issues/1495">Github issue</a>
-    you can read that the privacy policy of Google Fonts is not very clear.
+    The privacy policy of Google Fonts is not very clear.
 </p>
-<?php include 'footer.php';?>
+<ul>
+    <li><a href="https://github.com/google/fonts/issues/1495">Google Fonts - Github issue #1495</a></li>
+</ul>
+<?php include 'footer.php'; ?>
