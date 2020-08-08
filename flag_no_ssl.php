c65d769781d29fec3116ac20d38d2bde5b3e0763
Maurits van der Schee <maurits@vdschee.nl>
2020-08-08 17:10:41 +0200

    fix

---
+++ flag_no_ssl.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+<h1>No SSL</h1>
+<p>
+    It seems that there are requests done with 'http://' instead of 'https://' (no SSL).
+</p>
+<ul>
+    <li><a href="https://letsencrypt.org/">Let's Encrypt: A nonprofit Certificate Authority providing TLS certificates</a></li>
+</ul>
+<?php include 'footer.php'; ?>
