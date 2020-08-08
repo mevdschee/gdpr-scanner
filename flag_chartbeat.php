3cefcbd299a7d69d1437ca7915287a059b944f45
Maurits van der Schee <maurits@vdschee.nl>
2020-08-08 14:00:13 +0200

    fix

---
+++ flag_chartbeat.php
@@ -0,0 +1,10 @@
+<?php include 'header.php'; ?>
+<h1>Chartbeat Analytics</h1>
+<p>
+    This analytics service requires visitor's consent according to it's General Terms and Privacy Policy. See:
+</p>
+<ul>
+    <li><a href="https://chartbeat.com/terms/">Chartbeat Terms of Use</a></li>
+    <li><a href="https://chartbeat.com/privacy/">Chartbeat Privacy Policy</a></li>
+</ul>
+<?php include 'footer.php'; ?>
