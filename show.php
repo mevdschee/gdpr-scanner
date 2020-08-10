5610947f2c6a3fc3da3322969a803882b531e28c
Maurits van der Schee <maurits@vdschee.nl>
2020-08-10 16:51:17 +0200

    fix

--- show.php
+++ show.php
@@ -7,7 +7,7 @@ if (!file_exists("done/$date/$hash")) {
 }
 $reponse = json_decode(gzdecode(file_get_contents("done/$date/$hash")), true);
 ?>
-<?php include 'header.php'; ?>
+<?php include 'header.php';?>
 <style>
     th {
         text-align: left;
@@ -37,35 +37,37 @@ $reponse = json_decode(gzdecode(file_get_contents("done/$date/$hash")), true);
         <th>Domain<sup>1</sup></th>
         <th>Flags<sup>2</sup></th>
         <th>Ping<sup>3</sup></th>
-        <th>EU<sup>4</sup></th>
-        <th>Country<sup>5</sup></th>
-        <th>Organization<sup>6</sup></th>
+        <th>Hostname<sup>4</sup></th>
+        <th>EU<sup>5</sup></th>
+        <th>Country<sup>6</sup></th>
+        <th>Organization<sup>7</sup></th>
     </tr>
-    <?php foreach ($reponse['lines'] as $i => $line) : ?>
+    <?php foreach ($reponse['lines'] as $i => $line): ?>
         <tr>
-            <?php foreach ($line as $j => $cell) : ?>
+            <?php foreach ($line as $j => $cell): ?>
                 <td>
-                    <?php if (is_array($cell)) : ?>
-                        <?php foreach ($cell as $flag) : ?>
+                    <?php if (is_array($cell)): ?>
+                        <?php foreach ($cell as $flag): ?>
                             <?php echo "<a href=\"../flag_$flag.php\">$flag</a> "; ?>
-                        <?php endforeach; ?>
-                    <?php else : ?>
+                        <?php endforeach;?>
+                    <?php else: ?>
                         <?php echo htmlentities($cell) ?: '' ?>
-                    <?php endif; ?>
+                    <?php endif;?>
                 </td>
-            <?php endforeach; ?>
+            <?php endforeach;?>
         </tr>
-    <?php endforeach; ?>
+    <?php endforeach;?>
 </table>
 <p>
     1) Domain to which connections where made<br />
     2) Clickable flags with additional info<br />
     3) Ping time to the domain in milliseconds<br />
-    4) EU country for domain's IP address*<br />
-    5) Country of the domain's IP address*<br />
-    6) Organization of the domain's IP address*<br />
+    4) Reverse lookup for the domain's IP address<br />
+    5) EU country for domain's IP address*<br />
+    6) Country of the domain's IP address*<br />
+    7) Organization of the domain's IP address*<br />
     *) IP address information from: <a href="https://ip-api.com">ip-api.com</a>
 </p>
 <form action="../"><input type="submit" value="Close" /></form>
-<?php include 'footer.php'; ?>
+<?php include 'footer.php';?>
 <!-- <?php echo $filename; ?> -->
