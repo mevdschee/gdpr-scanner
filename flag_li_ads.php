63128fbc48a49e2b87f57653ca087e10f119294b
Maurits van der Schee <maurits@vdschee.nl>
2020-08-20 04:00:10 +0200

    update

---
+++ flag_li_ads.php
@@ -0,0 +1,6 @@
+<?php include 'header.php';?>
+<h1>LinkedIn Ads</h1>
+<p>
+    Ads are most likely not required for the functionality of the site and thus sharing IP address and user agent with Microsoft for this purpose requires consent.
+</p>
+<?php include 'footer.php';?>
